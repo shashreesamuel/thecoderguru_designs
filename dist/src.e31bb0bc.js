40e4f47e67861024f6378c6773997d4f4bb361a5
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-10-18 18:39:08 -0400

    Added some new code to empower the website

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8147,6 +8147,27 @@ function backToTop() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+$(function () {
+  $(".progress").each(function () {
+    var value = $(this).attr('data-value');
+    var left = $(this).find('.progress-left .progress-bar');
+    var right = $(this).find('.progress-right .progress-bar');
+
+    if (value > 0) {
+      if (value <= 50) {
+        right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)');
+      } else {
+        right.css('transform', 'rotate(180deg)');
+        left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)');
+      }
+    }
+  });
+
+  function percentageToDegrees(percentage) {
+    return percentage / 100 * 360;
+  }
+});
 },{"bootstrap":"../node_modules/bootstrap/dist/js/bootstrap.esm.js"}],"../../../.config/nvm/versions/node/v16.11.1/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -8175,7 +8196,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "43403" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45199" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
