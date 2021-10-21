7bddc07755d9a64d5ef3dc6525ade3eaf4a91071
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-10-21 10:00:33 -0400

    Installed rimraf

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -8213,7 +8213,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34707" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40337" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
