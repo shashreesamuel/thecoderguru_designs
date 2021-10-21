b0cb805ee7027e8b333faa55cde6a79f9f777760
TheCoderGuru <shashreeshachindrasamuel14@gmail.com>
2021-10-21 11:01:32 -0400

    Nice

---
+++ dist/contact.017db2f2.js
@@ -0,0 +1,51 @@
+'../node_modules/bootstrap/js';
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) mybutton.style.display = "block";
+    else mybutton.style.display = "none";
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+$(function() {
+    $(".progress").each(function() {
+        var value = $(this).attr('data-value');
+        var left = $(this).find('.progress-left .progress-bar');
+        var right = $(this).find('.progress-right .progress-bar');
+        if (value > 0) {
+            if (value <= 50) right.css('transform', 'rotate(' + percentageToDegrees(value) + 'deg)');
+            else {
+                right.css('transform', 'rotate(180deg)');
+                left.css('transform', 'rotate(' + percentageToDegrees(value - 50) + 'deg)');
+            }
+        }
+    });
+    function percentageToDegrees(percentage) {
+        return percentage / 100 * 360;
+    }
+});
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms).forEach(function(form) {
+        form.addEventListener('submit', function(event) {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
+
+//# sourceMappingURL=contact.017db2f2.js.map
